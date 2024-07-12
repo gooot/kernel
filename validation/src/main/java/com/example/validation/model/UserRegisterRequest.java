1fa8bd092bc94a3ddbde0a9ce38a5f98ef3d4b9f
이재윤 <kjd99002@naver.com>
2024-07-13 04:37:47 +0900

    learn to validation
    Test

--- validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
+++ validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -18,8 +19,9 @@ import java.time.LocalDateTime;
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRegisterRequest {
 
-    @NotBlank
     private String name;
+
+    private String nickName;
 
     @Size(min = 1,max = 12)
     @NotBlank
@@ -36,9 +38,34 @@ public class UserRegisterRequest {
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$",message = "휴대폰 번호 양식에 맞지 않음")
     private String phoneNumber;
 
-
     @FutureOrPresent
     private LocalDateTime registerAt;
+
+
+    // @AssertTrue(message = "name or nickName 은 존재 해야됩니다.")
+    // public boolean nameCheck() {
+    //
+    //     if (!name.isBlank()) {
+    //         return true;
+    //     }
+    //
+    //     if (!nickName.isBlank()) {
+    //         return true;
+    //     }
+    //
+    //     return false;
+    // }
+    @AssertTrue(message = "name or nickName 은 반드시 1개가 존재 해야됩니다.")
+    public boolean isNameCheck(){
+        if(Objects.nonNull(name) &&  !name.isBlank()){
+            return true;
+        }
+        if(Objects.nonNull(nickName) &&  !nickName.isBlank()){
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
